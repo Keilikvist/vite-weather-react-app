47f42c0f979efd1919a06fbe60ced50a00636e4f
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2026-03-29 18:14:03 -0500

    added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,21 @@ export default function App() {
       <div className="container">
         <Weather />
         <footer>
-          Coded by
+          Coded by{" "}
           <a
             href="https://www.linkedin.com/in/kayleigh-rundquist-00ab9a85/"
             target="_blank"
           >
             Kayleigh Rundquist
           </a>
-          , open-sourced on
+          , open-sourced on{" "}
           <a
             href="https://github.com/Keilikvist/vite-weather-react-app"
             target="_blank"
           >
             GitHub
           </a>
-          , and hosted on
+          , and hosted on{" "}
           <a href="https://vite-weather-react-app.netlify.app/" target="_blank">
             Netlify
           </a>
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -12,10 +12,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,13 +29,19 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
-            alt="partly sunny"
-          />
-          6º
+          <div className="clearfix">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+              alt="partly sunny"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
