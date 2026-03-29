88a7caed6cc796290fc2ef55f242672547efe1b8
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2026-03-29 17:21:17 -0500

    added bootstrap to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,31 @@ import SearchEngine from "./SearchEngine.jsx";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <SearchEngine />
+      <div className="container">
+        <h1>React Weather App</h1>
+        <SearchEngine />
+        <footer>
+          Coded by
+          <a
+            href="https://www.linkedin.com/in/kayleigh-rundquist-00ab9a85/"
+            target="_blank"
+          >
+            Kayleigh Rundquist
+          </a>
+          , open-sourced on
+          <a
+            href="https://github.com/Keilikvist/vite-weather-react-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          , and hosted on
+          <a href="https://vite-weather-react-app.netlify.app/" target="_blank">
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
