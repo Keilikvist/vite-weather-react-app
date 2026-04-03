df27177985bef736e0a2540e26983beb3969a7c8
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2026-04-03 11:45:37 -0500

    updated weather iconUrl to pull matching icon to weather info

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -14,8 +14,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-      iconUrl:
-        "https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg",
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       wind: response.data.wind.speed,
       city: response.data.city,
     });
