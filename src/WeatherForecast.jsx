8d8395fcb3a87399a3747f3276d0244f5c9fd2d9
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2026-04-09 18:36:09 -0500

    Started adding Forecast section

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Tue</div>
+
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19º</span>
+            <span className="WeatherForecast-temperature-min">10º</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
